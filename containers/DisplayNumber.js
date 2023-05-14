6ad1a6828674ed95e904612af868c798faf5fe1b
CHOI <121666486+CHOIYOUNGRYEOL@users.noreply.github.com>
2023-05-14 14:45:23 +0900

    Add files via upload
    Signed-off-by: CHOI <121666486+CHOIYOUNGRYEOL@users.noreply.github.com>

---
+++ containers/DisplayNumber.js
@@ -0,0 +1,26 @@
+import DisplayNumber from "../components/DisplayNumber";
+import {connect} from 'react-redux';
+function mapStateToProps(state){
+  return{
+    number:state.number
+  }
+}
+function mapDispatchToProps(){
+  return
+}
+export default connect(mapStateToProps,mapDispatchToProps)(DisplayNumber);
+
+// import React, { Component } from "react";
+// import store from "../store";
+// export default class extends Component{
+//   state = {number:store.getState().number}
+//   constructor(props){
+//       super(props);
+//       store.subscribe(function(){
+//           this.setState({number:store.getState().number});
+//       }.bind(this));
+//   }
+//   render(){
+//       return <DisplayNumber number={this.state.number}></DisplayNumber>
+//   }
+// } 
